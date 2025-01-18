bcad325220e14ba217ea0f89f9c3285cb5e7779b
gmarcinek <grzegorz.marcinek@gmail.com>
2025-01-18 03:29:31 +0100

    [TASK-0035] implement export to json, implement db import from file

--- src/api/db/export/useExportEvents.tsx
+++ src/api/db/export/useExportEvents.tsx
@@ -1,39 +1,46 @@
 import { CalendarEvent } from '@api/db/types';
 import { dateFormat } from '@components/Calendar/Calendar.helpers';
 import dayjs from 'dayjs';
-import { useGetAllEventsQuery } from '../events/useGetAllEventsQuery';
+import { useGetAllEventsMutation } from '../events/useGetAllEventsMutation';
 
 export const useExportEvents = () => {
-  const { query } = useGetAllEventsQuery(); // Korzystamy z zapytania do bazy
+  const { mutation, refetch } = useGetAllEventsMutation(); // Korzystamy z zapytania do bazy
 
-  const exportEventsToFile = () => {
-    if (query.isLoading) {
-      alert('Ładowanie danych...');
-      return;
+  const exportEventsToFile = async () => {
+    try {
+      await refetch(); // Czekamy na zakończenie refetch
+
+      if (mutation.isPending) {
+        alert('Ładowanie danych...');
+        return;
+      }
+
+      if (mutation.isError) {
+        alert('Wystąpił błąd podczas pobierania wydarzeń');
+        return;
+      }
+
+      const events: CalendarEvent[] = mutation.data || [];
+      console.log(mutation);
+
+      if (events.length === 0) {
+        alert('Brak wydarzeń do eksportu');
+        return;
+      }
+
+      // Konwertujemy wydarzenia na format JSON
+      const jsonBlob = new Blob([JSON.stringify(events)], {
+        type: 'application/json',
+      });
+
+      // Tworzymy link do pobrania
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(jsonBlob);
+      link.download = generateFileName();
+      link.click();
+    } catch (error) {
+      alert('Wystąpił nieoczekiwany błąd');
     }
-
-    if (query.error) {
-      alert('Wystąpił błąd podczas pobierania wydarzeń');
-      return;
-    }
-
-    const events: CalendarEvent[] = query.data || [];
-
-    if (events.length === 0) {
-      alert('Brak wydarzeń do eksportu');
-      return;
-    }
-
-    // Konwertujemy wydarzenia na format JSON
-    const jsonBlob = new Blob([JSON.stringify(events)], {
-      type: 'application/json',
-    });
-
-    // Tworzymy link do pobrania
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(jsonBlob);
-    link.download = generateFileName();
-    link.click();
   };
 
   return {
